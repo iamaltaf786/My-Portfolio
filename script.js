a3a4c00dc2ff5af032f9cd1ed8d26f957c76dcc1
Altaf Hussain <76874013+iamaltaf786@users.noreply.github.com>
2024-04-10 15:13:33 +0530

    Redesigned projects section and added some css

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Project Section
+const slides = document.querySelectorAll(".slide")
+var counter = 0;
+
+slides.forEach(
+  (slide, index) => {
+    slide.style.left = `${index * 100}%`
+  }
+)
+
+const goPrev = () => {
+  counter--;
+  if (counter == -1) counter = 2;
+  slideImage();
+}
+
+const goNext = () => {
+  counter++;
+  if (counter == 3) counter = 0;
+  slideImage();
+}
+
+const slideImage = () => {
+  slides.forEach(
+    (slide) => {
+      slide.style.transform = `translateX(-${counter * 100}%)`
+    }
+  )
+}
